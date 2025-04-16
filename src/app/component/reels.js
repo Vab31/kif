bc24835355af1a90f4bcbba883802a31cc3d00eb
Vaibhav Singh <singhvaibhav654@gmail.com>
2025-04-16 18:11:43 +0530

    second commit

--- src/app/component/reels.js
+++ src/app/component/reels.js
@@ -46,60 +46,107 @@ export default function Reels() {
   return (
     <div>
 {!showForm && (
+  <div>
 
-<div>
-
+{/* Section 1 */}
 <section className="text-gray-600 bg-gray-200 body-font" data-aos="fade-right">
   <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-amber-700">Before they sold out
-        <br className="hidden lg:inline-block"/>readymade gluten
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-amber-700">
+        Before they sold out
+        <br className="hidden lg:inline-block" />
+        readymade gluten
       </h1>
-      <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+      <p className="mb-8 leading-relaxed">
+        Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. 
+        Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.
+      </p>
       <div className="flex justify-center">
-        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg" >Watch More</button>
+        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          Watch More
+        </button>
       </div>
     </div>
-    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 rounded">
-    <iframe width="360" className='rounded' height="640" src="https://www.youtube.com/embed/9ymhsjd2VeE" frameBorder="0" allowFullscreen></iframe>
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-full">
+      <div className="relative w-full h-[240px] md:h-[360px] lg:h-[600px]">
+        <iframe
+          className="absolute top-0 left-0 w-full h-full rounded"
+          src="https://www.youtube.com/embed/9ymhsjd2VeE"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   </div>
 </section>
 
-<section class="text-gray-600 body-font" data-aos="fade-left">
-  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-    <iframe width="360" className='rounded' height="640" src="https://www.youtube.com/embed/9ymhsjd2VeE" frameBorder="0" allowFullscreen></iframe>
+{/* Section 2 */}
+<section className="text-gray-600 body-font" data-aos="fade-left">
+  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-full mb-10 md:mb-0">
+      <div className="relative w-full h-[240px] md:h-[360px] lg:h-[600px]">
+        <iframe
+          className="absolute top-0 left-0 w-full h-full rounded"
+          src="https://www.youtube.com/embed/9ymhsjd2VeE"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
-    <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-amber-700">Before they sold out
-        <br class="hidden lg:inline-block"/>readymade gluten
+    <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-amber-700">
+        Before they sold out
+        <br className="hidden lg:inline-block" />
+        readymade gluten
       </h1>
-      <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
-      <div class="flex justify-center">
-        <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg" >Watch More</button>
-     </div>
+      <p className="mb-8 leading-relaxed">
+        Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. 
+        Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.
+      </p>
+      <div className="flex justify-center">
+        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          Watch More
+        </button>
+      </div>
     </div>
   </div>
 </section>
 
+{/* Section 3 */}
 <section className="text-gray-600 bg-gray-200 body-font" data-aos="fade-right">
-  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center ">
+  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-amber-700">Before they sold out
-        <br className="hidden lg:inline-block"/>readymade gluten
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-amber-700">
+        Before they sold out
+        <br className="hidden lg:inline-block" />
+        readymade gluten
       </h1>
-      <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+      <p className="mb-8 leading-relaxed">
+        Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. 
+        Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.
+      </p>
       <div className="flex justify-center">
-        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg" >Watch More</button>
+        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          Watch More
+        </button>
       </div>
     </div>
-    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 rounded">
-    <iframe width="360" className='rounded' height="640" src="https://www.youtube.com/embed/9ymhsjd2VeE" frameBorder="0" allowFullscreen></iframe>
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-full">
+      <div className="relative w-full h-[240px] md:h-[360px] lg:h-[600px]">
+        <iframe
+          className="absolute top-0 left-0 w-full h-full rounded"
+          src="https://www.youtube.com/embed/9ymhsjd2VeE"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   </div>
 </section>
+
 </div>
+
+
 )}
 
 
