8db7e23555622f38ce6db5e6ea68e94e8b5452b9
Vaibhav Singh <singhvaibhav654@gmail.com>
2025-05-01 16:46:22 +0530

    photo update

--- src/app/component/saesha.js
+++ src/app/component/saesha.js
@@ -5,13 +5,13 @@ export default function Saesha() {
     <div>
         <section className="text-gray-600 body-font bg-white" data-aos="fade-up">
   <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-    <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"/>
+    <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="/images/ss.jpeg"/>
     <div className="text-center lg:w-2/3 w-full">
       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-bold text-green-700">About Saesha</h1>
       <p className="mb-8 leading-relaxed">Hi! I’m Saesha Khosla, a 9th-grade student who believes that music has the power to change lives. I’m turning my passion for playing the piano into a purpose-driven mission — raising funds to support individuals who are differently abled. Through my performances, I aim to empower them with economically sustainable entrepreneurial ideas, helping them build a path toward financial independence and a brighter future.</p>
-      <div className="flex justify-center">
+      {/* <div className="flex justify-center">
         <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Donate</button>
-      </div>
+      </div> */}
     </div>
   </div>
 </section>
